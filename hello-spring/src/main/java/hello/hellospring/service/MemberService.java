f0e918c864a62c1e7462f40e145c8024bd0d5951
nhkiiim <nhkiiim@naver.com>
2023-01-20 16:44:59 +0900

    Feat: 회원 전체 조회 기능 추가

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -27,4 +27,14 @@ public class MemberService {
                         });
     }
 
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
