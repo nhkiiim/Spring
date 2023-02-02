33c0d1d48f460d3349f1f72621be508b687f753d
nhkiiim <nhkiiim@naver.com>
2023-02-02 21:57:45 +0900

    Add: AOP 적용 전

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -25,9 +25,17 @@ public class MemberService {
      */
     @Transactional // JPA 사용 시 트랜잭션안에서 실행되어야 함
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println(timeMs);
+        }
     }
 
     private void validateDuplicateMember(Member member) {
